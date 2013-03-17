826e52539db5afc2a76d8ba21ed3bd790ab9c05d
sdremillard@gmail.com <sdremillard@Stephens-Macbook-Pro.(none)>
2013-03-17 13:51:37 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slopedreams::Application.config.secret_token = '12cd32bf89cd2a161f14577e04d711713b2eb7e4d212ae971caf09d54d7ce43f4d2a22e696920548ade5e58f1cd5fe88ef3be5ead8994e3fd57127781c05c646'
